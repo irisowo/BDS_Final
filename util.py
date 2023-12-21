609535b69516d8af3de45efdefad2537d3b31ae0
irisowo <irischiu0106@gmail.com>
2023-12-22 00:58:53 +0800

    add util.py

---
+++ util.py
@@ -0,0 +1,21 @@
+import json
+from PIL import Image
+import streamlit as st 
+from streamlit_lottie import st_lottie
+
+
+def import_json(path):
+    with open(path, "r", encoding="utf8", errors="ignore") as file:
+        url = json.load(file)
+        return url
+
+
+def render_sider():
+    with st.sidebar.container():
+        image = Image.open(r"images/character.png")  
+        st.image(image, use_column_width=True)
+
+
+def render_banner(file_path):
+    data_oracle = import_json(file_path)
+    st_lottie(data_oracle, height = 400, key = "oracle")
